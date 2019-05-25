831285059efc33801eeae15d5852522e29ff3627
Aditya <49444558+adityarohman340@users.noreply.github.com>
2019-05-25 21:07:22 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="bootstrap-4.3.1-dist/bootstrap-4.3.1-dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="style.css">
-    <title>Register Form</title>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <title>Form Registerasi Developer</title>
 </head>
 <body>
     <div class="user">
